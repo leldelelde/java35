1a2cb8b69241c08d62be06c1cc4cd92fb9e29a83
Lelde Zemberga <zembergalelde@gmail.com>
2023-02-10 21:34:01 +0200

    Variables and data types

---
+++ src/DataTypes.java
@@ -0,0 +1,37 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Integer Data type
+        byte num1 = 100;
+        short num2 = 1000;
+        int num3 = 3425354;
+        long num4 = 4134253453L;
+
+        //Floating point data type
+        float num5 = 12312.243f;
+        double num6 = 24234.2324324586522844d;
+
+        //Boolean data type
+        boolean redLightSwitch = false;
+        boolean blackLightSwitch = true;
+
+        //Character data type
+        char char1 = '%';
+        char char2 = 'y';
+        char char3 = '0';
+        char char4 = '\n';
+
+        //String data type
+        String text1 = "This is very good text";
+
+        //Print out variable values
+        System.out.println(num1);
+        System.out.println(num6);
+        System.out.println(redLightSwitch);
+        System.out.println(blackLightSwitch);
+        System.out.println(char4);
+        System.out.println(char2);
+        System.out.println(text1);
+        
+
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Lelde!");
-    System.out.println("Hello another time");
+        System.out.println("Hello She Goes Tech");
+        //This is single-line comment
+        /*
+        This is
+        Multi-line
+        Comment
+         */
+
     }
 }
