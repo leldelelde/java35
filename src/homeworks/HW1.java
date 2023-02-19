5a5afbcddf82f6d250f79b0e624d84376c4ef7ec
Lelde Zemberga <zembergalelde@gmail.com>
2023-02-19 17:24:44 +0200

    Homeworks 1 and 2

--- src/homeworks/HW1.java
+++ src/homeworks/HW1.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HW1 {
     public static void main(String[] args) {
         // Using scanner ask user to enter name and year of birth
@@ -7,5 +9,17 @@ public class HW1 {
         //      John, 2000
         // Print out:
         //      "Your name is John and you are 22 years old"
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter your year of birth");
+        int year = scanner.nextInt();
+
+        System.out.println("Your name is " + name + " and you are " + (2022 - year) + " years old");
+
+
+
     }
 }
--- src/homeworks/HW2.java
+++ src/homeworks/HW2.java
@@ -1,11 +1,25 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HW2 {
     public static void main(String[] args) {
         // Using scanner read speed in mp/h
         // Calculate and output speed in km/h
         // Example with input 122.7 mp/h
         // Output --> 122.7 mp/h in km/h would be equal to 197.46651
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter speed in mp/h");
+        float speed = scanner.nextFloat();
+
+        System.out.println(speed + " mp/h in km/h would be equal to " + (speed * 1.609344));
+
+
+
+
+
+
 
     }
 }
