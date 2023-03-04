a13a4da37a2ef511429f20982cf97921176d84f9
Lelde Zemberga <zembergalelde@gmail.com>
2023-03-04 13:36:18 +0200

    Completed HW7

--- src/homeworks/HW7.java
+++ src/homeworks/HW7.java
@@ -1,5 +1,6 @@
 package homeworks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HW7 {
@@ -9,17 +10,27 @@ public class HW7 {
 
         System.out.println("Enter the required size of the array:");
         int sizeOfArray = scanner.nextInt();
-
-        String [] ArrayFromInput = new String[sizeOfArray];
+        int [] ArrayFromInput = new int[sizeOfArray];
 
         //Length of array
         //System.out.println(ArrayFromInput.length);
+        System.out.println("Enter the elements of the array one by one");
 
-        int i = 0;
-        do {
+        for (int i = 0; i < sizeOfArray; i++){
             System.out.println("Please enter element number: " + (i+1));
             int elementNr = scanner.nextInt();
-            i++;
-        }while (i != sizeOfArray );
+            ArrayFromInput[i] = elementNr;
+        }
+        System.out.print("Source Array: ");
+        for (int i = 0; i < sizeOfArray; i++) {
+            System.out.print(ArrayFromInput[i] + " ");
+        }
+        System.out.println();
+
+        int sum = 0;
+        for (int k = 0; k < sizeOfArray; k++) {
+            sum = sum + ArrayFromInput[k];
+        }
+        System.out.println("Sum of all elements : " + sum);
     }
 }
