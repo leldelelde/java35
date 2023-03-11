9ea6aba887818c8dce1283d82cae6f67e733b890
Lelde Zemberga <zembergalelde@gmail.com>
2023-03-11 13:58:52 +0200

    Completed HW10 on reverse array

--- src/homeworks/HW10.java
+++ src/homeworks/HW10.java
@@ -8,10 +8,12 @@ public class HW10 {
         int [] myArray = {1,2,3,4,5,6,20,7,8,9,10,11,12};
         System.out.println("Original array: " + Arrays.toString(myArray));
 
-        for(int i = 0; i < myArray.length; i++){
-            myArray[i] = myArray[myArray.length -(i+1)];
-            (myArray[myArray.length-(i+1)]) = myArray[i];
+        for(int i = 0; i < myArray.length/2; i++){
+            int reverse = myArray[i];
+            myArray[i] = myArray[myArray.length -1 -i];
+            myArray[myArray.length-1-i] = reverse;
         }
         System.out.print("Reverse array: " + Arrays.toString(myArray));
+
     }
 }
