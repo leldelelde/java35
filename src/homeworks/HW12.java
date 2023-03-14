c4f9e37e5eadb38f385cd805573344408c7ae3e8
Lelde Zemberga <zembergalelde@gmail.com>
2023-03-14 19:54:03 +0200

    Improved homework 12

--- src/homeworks/HW12.java
+++ src/homeworks/HW12.java
@@ -7,27 +7,22 @@ public class HW12 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a name");
-        String inputName = scanner.nextLine().toLowerCase().trim();
+        String inputValue = scanner.nextLine().toLowerCase().trim();
         int k = 0;
-        boolean palindrome = true;
 
-        for (int i = 0; i < inputName.length()/2; i++) {
-            if (inputName.charAt(i) == inputName.charAt(inputName.length() - 1 - i)) {
-                // System.out.println(inputName.charAt(i) == inputName.charAt(inputName.length() - 1 - i));
+        for (int i = 0; i < inputValue.length()/2; i++) {
+            if (inputValue.charAt(i) == inputValue.charAt(inputValue.length() - 1 - i)) {
+                k=0;
+            } else {
                 k++;
-                // System.out.println(k);
-            } else {
-                k--;
             }
         }
-            if (k <= 0) {
-                System.out.println(inputName.toLowerCase().trim() + " is not a palindrome");
+            if (k > 0) {
+                System.out.println(inputValue + " is not a palindrome");
             } else {
-                System.out.println(inputName.toLowerCase().trim() + " is a palindrome");
+                System.out.println(inputValue + " is a palindrome");
             }
-
-        }
-
+    }
 }
 
 
